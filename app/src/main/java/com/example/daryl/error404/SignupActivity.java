b680aef03a900b773eedf9df0e5b0ee35800ed35
ddahoo <dahoo097@gmail.com>
2018-10-04 18:21:41 -0400

    Added click handler for signup button

--- app/src/main/java/com/example/daryl/error404/SignupActivity.java
+++ app/src/main/java/com/example/daryl/error404/SignupActivity.java
@@ -2,6 +2,9 @@ package com.example.daryl.error404;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void signUpClick(View v)
+    {
+        EditText firstName = (EditText) findViewById(R.id.firstNameEdit);
+        EditText lastName = (EditText) findViewById(R.id.lastNameEdit);
+        EditText dob = (EditText) findViewById(R.id.dobEdit);
+        EditText email = (EditText) findViewById(R.id.emailEdit);
+        EditText password = (EditText) findViewById(R.id.passwordEdit);
+        Spinner accountType = (Spinner) findViewById(R.id.accountTypeSpinner);
+
+        
     }
 }
