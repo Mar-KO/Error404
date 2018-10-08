ad91aa4e25af89106d6a2146bc19c5b96a122e8c
Jean-Luc Blais-Amyot <jblai083@uottawa.ca>
2018-10-07 22:59:39 -0400

    hello

--- app/src/main/java/com/example/daryl/error404/Help.java
+++ app/src/main/java/com/example/daryl/error404/Help.java
@@ -1,7 +1,9 @@
 package com.example.daryl.error404;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Help extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void backFromHelpButton(View view){
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivityForResult(intent, 0);
     }
 }
