ce31308e8b93e7d579d3c262263e2daa2235265f
ddahoo <dahoo097@gmail.com>
2018-10-22 14:03:50 -0400

    Added code to add users into Firebase

--- app/src/main/java/com/example/daryl/error404/SignupActivity.java
+++ app/src/main/java/com/example/daryl/error404/SignupActivity.java
@@ -5,8 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText firstNameEdit;
+    EditText lastNameEdit;
+    EditText dobEdit;
+    EditText emailEdit;
+    EditText passwordEdit;
+    Spinner accountTypeSpinner;
+
+    DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +30,31 @@ public class SignupActivity extends AppCompatActivity {
 
     public void signUpClick(View v)
     {
-        EditText firstName = (EditText) findViewById(R.id.firstNameEdit);
-        EditText lastName = (EditText) findViewById(R.id.lastNameEdit);
-        EditText dob = (EditText) findViewById(R.id.dobEdit);
-        EditText email = (EditText) findViewById(R.id.emailEdit);
-        EditText password = (EditText) findViewById(R.id.passwordEdit);
-        Spinner accountType = (Spinner) findViewById(R.id.accountTypeSpinner);
+        firstNameEdit = (EditText) findViewById(R.id.firstNameEdit);
+        lastNameEdit = (EditText) findViewById(R.id.lastNameEdit);
+        dobEdit = (EditText) findViewById(R.id.dobEdit);
+        emailEdit = (EditText) findViewById(R.id.emailEdit);
+        passwordEdit = (EditText) findViewById(R.id.passwordEdit);
+        accountTypeSpinner = (Spinner) findViewById(R.id.accountTypeSpinner);
+
+        db = FirebaseDatabase.getInstance().getReference();
+        String firstName = firstNameEdit.getText().toString();
+        String lastName = lastNameEdit.getText().toString();
+        String dob = dobEdit.getText().toString();
+        String email = emailEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
+        String accountType = accountTypeSpinner.getSelectedItem().toString();
+
+        Map<String, Object> info = new HashMap<>();
+
+        info.put("firstName", firstName);
+        info.put("lastName", lastName);
+        info.put("dateOfBirth", dob);
+        info.put("email", email);
+        info.put("password", password);
+        info.put("accountType", accountType);
+
+        db.child("users").setValue(info);
 
         
     }
