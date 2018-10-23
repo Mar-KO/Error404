031bc817a8faf2226418bd76d37995d1f416fe7a
Lydia-Fen Leduc-Chapman <lledu102@uottawa.ca>
2018-10-23 17:53:15 -0400

    My version of the 3 classes

---
+++ app/src/main/java/com/example/daryl/error404/Account.java
@@ -0,0 +1,76 @@
+package com.example.daryl.error404;
+
+import android.widget.DatePicker;
+
+import java.util.Date;
+
+public class Account {
+
+    //Instances de classes
+    String typeOfAccount;
+    String passeword;
+    String firstName;
+    String lastName;
+    DatePicker dateOfBirth;
+    String email;
+
+    public Account(String typeOfAccount, String passeword, String firstName, String lastName, DatePicker dateOfBirth, String email){
+        this.typeOfAccount = typeOfAccount;
+        this.passeword = passeword;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+
+    }
+
+    //Setters methods
+    //In case the user has to change their info
+
+    public void setTypeOfAccount(String newTypeOfAccount){
+        typeOfAccount = newTypeOfAccount;
+
+    }
+    public void setPasseword(String newPasseword){
+        passeword = newPasseword;
+    }
+
+    public void setFirstName(String newFirstName){
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName){
+        lastName = newLastName;
+    }
+    public void setDateOfBirth(DatePicker newdateOfBirth){
+        dateOfBirth = newdateOfBirth;
+    }
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+
+    //Getters methods
+    //To get the info of the user
+
+    public String getTypeOfAccount(){
+        return typeOfAccount;
+    }
+    public String getPasseword() {
+        return passeword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public DatePicker getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- app/src/main/java/com/example/daryl/error404/MainActivity.java
+++ app/src/main/java/com/example/daryl/error404/MainActivity.java
@@ -1,26 +1,27 @@
 package com.example.daryl.error404;
 
+import android.view.View;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void sighUpOnClick(View view){
-        Intent intent= new Intent(this, SignupActivity.class);
-        startActivityForResult(intent, 0);
-    }
-    public void helpOnClick(View view){
-        Intent intent= new Intent(this, Help.class);
-        startActivityForResult(intent, 0);
-    }
 
+    public void sighUpOnClick (View view){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivityForResult(intent, 0);
+    }
 
 }
+
