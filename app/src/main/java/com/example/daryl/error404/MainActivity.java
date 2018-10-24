ed5888520406473c6ae7b4c3e4fe4f52573c1630
Jean-Luc Blais-Amyot <jblai083@uottawa.ca>
2018-10-23 23:40:42 -0400

    welcome activity with name and role

--- app/src/main/java/com/example/daryl/error404/MainActivity.java
+++ app/src/main/java/com/example/daryl/error404/MainActivity.java
@@ -4,24 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText email;
+    EditText password;
+    DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseDatabase.getInstance().getReference();
     }
 
     public void sighUpOnClick(View view){
         Intent intent= new Intent(this, SignupActivity.class);
-        startActivityForResult(intent, 0);
+        startActivity(intent);
     }
 
     public void WelcomeOnClick(View view){
-        Intent intent= new Intent(this, WelcomeActivity.class);
+        email= findViewById(R.id.userName);
+        password=findViewById(R.id.password);
+        String sEmail,sPassword;
+        sEmail=email.getText().toString();
+        sPassword=password.getText().toString();
 
-        startActivityForResult(intent, 0);
+
+        Intent intent= new Intent(this, WelcomeActivity.class);
+        intent.putExtra("EMAIL", sEmail);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/daryl/error404/SignupActivity.java
+++ app/src/main/java/com/example/daryl/error404/SignupActivity.java
@@ -1,10 +1,14 @@
 package com.example.daryl.error404;
 
+//import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,25 +23,25 @@ public class SignupActivity extends AppCompatActivity {
     EditText emailEdit;
     EditText passwordEdit;
     Spinner accountTypeSpinner;
-
     DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        db = FirebaseDatabase.getInstance().getReference();
     }
 
     public void signUpClick(View v)
     {
-        firstNameEdit = (EditText) findViewById(R.id.firstNameEdit);
-        lastNameEdit = (EditText) findViewById(R.id.lastNameEdit);
-        dobEdit = (EditText) findViewById(R.id.dobEdit);
-        emailEdit = (EditText) findViewById(R.id.emailEdit);
-        passwordEdit = (EditText) findViewById(R.id.passwordEdit);
-        accountTypeSpinner = (Spinner) findViewById(R.id.accountTypeSpinner);
+        firstNameEdit =  findViewById(R.id.firstNameEdit);
+        lastNameEdit =  findViewById(R.id.lastNameEdit);
+        dobEdit =  findViewById(R.id.dobEdit);
+        emailEdit =  findViewById(R.id.emailEdit);
+        passwordEdit =  findViewById(R.id.passwordEdit);
+        accountTypeSpinner =  findViewById(R.id.accountTypeSpinner);
 
-        db = FirebaseDatabase.getInstance().getReference();
+
         String firstName = firstNameEdit.getText().toString();
         String lastName = lastNameEdit.getText().toString();
         String dob = dobEdit.getText().toString();
@@ -46,16 +50,41 @@ public class SignupActivity extends AppCompatActivity {
         String accountType = accountTypeSpinner.getSelectedItem().toString();
 
         Map<String, Object> info = new HashMap<>();
+        if(verifyInfo(firstName,lastName,password,email,dob)) {
+            info.put("firstName", firstName);
+            info.put("lastName", lastName);
+            info.put("dateOfBirth", dob);
+            info.put("email", email);
+            info.put("password", password);
+            info.put("accountType", accountType);
 
-        info.put("firstName", firstName);
-        info.put("lastName", lastName);
-        info.put("dateOfBirth", dob);
-        info.put("email", email);
-        info.put("password", password);
-        info.put("accountType", accountType);
+            db.child("users").child(email).setValue(info);
 
-        db.child("users").child(email).setValue(info);
+            Intent i = new Intent(this, WelcomeActivity.class);
+            i.putExtra("FIRSTNAME", firstName);
+            i.putExtra("ROLE", accountType);
+            startActivity(i);
+        }
+        else{
 
+            Toast.makeText(this,"Your infomations are not entered properly", Toast.LENGTH_LONG).show();
+        }
         
+    }
+
+    private boolean verifyAdmin(){
+        boolean found=false;
+
+
+
+        return found;
+    }
+
+    private boolean verifyInfo(String fName, String lName, String pWord, String email, String dob) {
+        return !(fName == null || fName.isEmpty() ||
+                lName == null || lName.isEmpty() ||
+                pWord == null || pWord.isEmpty() ||
+                email == null || email.isEmpty() ||
+                dob == null || dob.isEmpty());
     }
 }
